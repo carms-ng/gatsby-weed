c3db476559a03041067245748b65b3aa21258cbc
Carms Ng <carmenhyng89@gmail.com>
2021-01-21 20:24:51 -0500

    [ADD] onClick effect to randomize image position

--- src/pages/amarente.js
+++ src/pages/amarente.js
@@ -32,15 +32,27 @@ export default function AmarentePage() {
 
   const explode = () => {
     const explodees = document.querySelectorAll(".explodee");
-    console.log(explodees);
+    // set window height
+    let pageHeight = 0;
+    explodees.forEach((elem) => {
+      pageHeight += elem.clientHeight;
+    })
+    // for each of the element set top and left style randomly
+    explodees.forEach((elem) => {
+      elem.style.top = rand(pageHeight, 0) + "px";
+      elem.style.left = rand(80, 20) + "vw";
+    })
   }
 
   const ExplodeStyles = styled.div`
+    padding: 100px;
+
     .explodee {
       top: 50vh;
       left: 50vw;
       transform: translate(-50%, -50%);
-      box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3)
+      box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+      transition: all 1s ease-in-out;
     }
   `;
 
@@ -49,7 +61,7 @@ return (
       <ExplodeStyles onClick={explode}>
         {data.allFile.edges.map(({ node } ) => (
           <Img
-            style={{ position: "absolute", overflow: "visible", width: `${rand(60, 10)}vw`}}
+            style={{ position: "absolute", overflow: "visible", width: `${rand(70, 30)}vmin`}}
             // , zIndex: `${rand(20, 1)}`
             className="explodee"
             fluid={node.childImageSharp.fluid}
