3913a42b0869282c3b68763d4bb22865bab0d0f5
Carms Ng <carmenhyng89@gmail.com>
2021-01-21 19:47:14 -0500

    [UPD] randomize image size

--- src/pages/amarente.js
+++ src/pages/amarente.js
@@ -1,6 +1,8 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import Img from "gatsby-image"
+import Layout from '../components/Layout';
+import styled from 'styled-components';
 
 export default function AmarentePage() {
   const data = useStaticQuery(graphql`
@@ -22,14 +24,40 @@ export default function AmarentePage() {
         }
       }
     }
-
   `)
-  return (
-    <div>
-      <h1>0.1 CONCRETE & PLANT RELATIONSHIPS</h1>
-      {data.allFile.edges.map(({ node } ) => (
-        <Img fluid={node.childImageSharp.fluid} />
-      ))}
-    </div>
+  
+  const rand = (max, min) => {
+    return Math.floor(Math.random() * (max-min) + min);
+  }
+
+  const explode = () => {
+    const explodees = document.querySelectorAll(".explodee");
+    console.log(explodees);
+  }
+
+  const ExplodeStyles = styled.div`
+    .explodee {
+      top: 50vh;
+      left: 50vw;
+      transform: translate(-50%, -50%);
+      box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3)
+    }
+  `;
+
+return (
+    <Layout>
+      <ExplodeStyles onClick={explode}>
+        {data.allFile.edges.map(({ node } ) => (
+          <Img
+            style={{ position: "absolute", overflow: "visible", width: `${rand(60, 10)}vw`}}
+            // , zIndex: `${rand(20, 1)}`
+            className="explodee"
+            fluid={node.childImageSharp.fluid}
+            key={node.base}
+            alt={node.base.split(".")[0] }
+          />
+        ))}
+      </ExplodeStyles>
+    </Layout>
   );
 }
