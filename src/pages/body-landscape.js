fa864b22b5c909d43d5f5803581a7ee62e099b13
Alex <xelanawotka@gmail.com>
2021-01-23 23:28:43 -0500

    Add sections and update image names

--- src/pages/body-landscape.js
+++ src/pages/body-landscape.js
@@ -26,16 +26,63 @@ export default function SubPageFive() {
 
   return (
     <Layout>
-      <div className="grid grid-cols-2">
+      <section className="container">
+        <div className="flex container">   
+          <Img
+            fluid={jpgs[0].node.childImageSharp.fluid}
+            alt={jpgs[0].node.base.split(".")[0]}
+            className="container"
+            />
+          <Img
+            fluid={jpgs[1].node.childImageSharp.fluid}
+            alt={jpgs[1].node.base.split(".")[0]}
+            className="container"
+            />
+          <Img
+            fluid={jpgs[2].node.childImageSharp.fluid}
+            alt={jpgs[2].node.base.split(".")[0]}
+            className="container"
+            />
 
-      </div>
+        </div>
 
-      <div className="container justify-self-end md:justify-self-center px-4 max-w-1/2 md:max-w-3/10">     
-        <Img
-          fluid={jpgs[1].node.childImageSharp.fluid}
-          alt={jpgs[1].node.base.split(".")[0]}
-          />
-      </div>
+        <div className="grid grid-cols-2">
+          <Img
+            fluid={jpgs[3].node.childImageSharp.fluid}
+            alt={jpgs[3].node.base.split(".")[0]}
+            className="container"
+            />
+
+        </div>
+
+        <div className="grid grid-cols-3">
+
+        </div>
+
+        <div className="grid grid-cols-2">
+
+        </div>
+        <div className="flex">
+
+        </div>
+        
+        <div className="flex">
+
+        </div>
+        
+        <div className="flex">
+
+        </div>
+
+        <div className="container justify-self-end md:justify-self-center px-4 max-w-1/2 md:max-w-3/10"> 
+          <div className="container">   
+            <Img
+              fluid={jpgs[1].node.childImageSharp.fluid}
+              alt={jpgs[1].node.base.split(".")[0]}
+              />
+          </div> 
+        </div>
+      </section>
     </Layout>
   );
 }
