e5efda84b4161424a3ac9520a3b096d5402160bd
Alex <xelanawotka@gmail.com>
2021-01-27 13:12:47 -0500

    [ADD] Third landscape image and page

--- src/pages/third-landscape.js
+++ src/pages/third-landscape.js
@@ -1,9 +1,103 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
+import Layout from '../components/Layout';
+import Nav from '../components/Nav';
+import gif_45 from '../assets/gifs/045_third-landscape.gif';
+import styled from 'styled-components';
+import { rand } from '../utils/helper';
+import { explode } from '../utils/effect';
+
+const ExplodeStyles = styled.div`
+  overflow: hidden;
+  padding: 100px;
+  .explodee {
+    top: 50vh;
+    left: 50vw;
+    transform: translate(-50%, -50%);
+    transition: all 1s ease-in-out;
+  }
+  .floating {
+    animation-name: floating;
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    transform: translate3d(0, 0, 0);
+    backface-visibility: hidden;
+    perspective: 1000px;
+  }
+  @keyframes floating {
+    10%,
+    90% {
+      transform: translate3d(-1px, 0, 0);
+    }
+    20%,
+    80% {
+      transform: translate3d(2px, 0, 0);
+    }
+    30%,
+    50%,
+    70% {
+      transform: translate3d(-4px, 0, 0);
+    }
+    40%,
+    60% {
+      transform: translate3d(4px, 0, 0);
+    }
+  }
+`;
 
 export default function SubPageFour() {
+  const data = useStaticQuery(graphql`
+    query GetImagesThirdLandscape {
+      allFile(filter: { absolutePath: { regex: "/third_landscape/" } }) {
+        edges {
+          node {
+            base
+            childImageSharp {
+              fluid(maxWidth: 2048, quality: 80) {
+                aspectRatio
+                base64
+                sizes
+                src
+                srcSet
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <div>
-      <h1>SubPageFour - Randomized</h1>
-    </div>
+    <Layout>
+      <Nav />
+      <ExplodeStyles onClick={explode}>
+        {data.allFile.edges.map(({ node }) => (
+          <Img
+            style={{
+              position: 'absolute',
+              overflow: 'visible',
+              width: `${rand(70, 30)}vmin`,
+              zIndex: rand(20, 1),
+            }}
+            className="explodee"
+            fluid={node.childImageSharp.fluid}
+            key={node.base}
+            alt={node.base.split('.')[0]}
+          />
+        ))}
+        <img
+          src={gif_45}
+          alt="Third Landscape"
+          style={{
+            width: '20%',
+            margin: '20rem auto 0',
+            zIndex: rand(20, 1),
+          }}
+          className="explodee"
+        />
+      </ExplodeStyles>
+    </Layout>
   );
 }
