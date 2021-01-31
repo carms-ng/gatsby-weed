6cb4c31d10693660a14e062e0aa8f645f8443eb4
Carms Ng <carmenhyng89@gmail.com>
2021-01-30 22:35:36 -0500

    [WIP] 404 page remove test video

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Video from '../components/Video';
-import sample from '../assets/videos/3.mp4';
 
 export default function FourOhFourPage() {
   return (
     <>
       <p>Page doesn't exist!</p>
-      {/* passing in the cssStyle and src as props */}
-      <Video style={{ width: `30vw` }} src={sample}/>
     </>
   );
 }
