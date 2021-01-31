67054246fd768a16efc7a2374e20becd941f005c
Alex <xelanawotka@gmail.com>
2021-01-30 19:36:33 -0500

    [ADD] Site metadata description + image

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,9 @@ module.exports = {
   pathPrefix: `/gatsby-weed`,
   siteMetadata: {
     title: "What is a weed?",
+    description:
+    "A collaborative online project co-created by Eve Tagny and Io Makandal.",
+    image: "/images/what-is-a-weed.jpg",
   },
   plugins: [
     "gatsby-plugin-styled-components",
