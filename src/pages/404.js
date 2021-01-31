1f4bfe63f71eea596183b013f614a6805478ed6a
Alex <xelanawotka@gmail.com>
2021-01-30 21:52:54 -0500

    [UPD] 404 copy

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,11 +14,10 @@ export default function FourOhFourPage() {
         <title>404 Not Found | What is a Weed?</title>
       </Helmet>
       <NavSubpage />
-      <section className="space-y-12 mb-12">
-        <p className="text-3xl">Whoops, this page doesn't exist.</p>
-        <p className="text-xl">Take me back 
-        <Link to="/" className="mx-1 text-green hover:underline">home</Link>
-        please.
+      <section className="space-y-6 mb-12">
+        <p className="text-2xl">Hmm, looks like the weeds have led you to a page that doesn't exist.</p>
+        <p className="text-xl">Let's go back 
+        <Link to="/" className="ml-1 text-green hover:underline">home</Link>.
         </p>
         {/* passing in the cssStyle and src as props */}
         <Video className="max-w-3/4 md:max-w-2/5" src={sample}/>
